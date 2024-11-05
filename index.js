90d2a0633264a08f7884882ed083cce0f0c5dce1
Nikolay <103648794+gilotin@users.noreply.github.com>
2024-11-05 11:15:38 +0200

    feat: added the rating logic

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const button = document.querySelector(".rating-card__form-button");
+const form = document.querySelector(".rating-card__form");
+const ratingCard = document.querySelector(".rating-card");
+const resultCard = document.querySelector(".result-card");
+const result = document.querySelector(".result-card__paragraph");
 
-button.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const rating = formData.get("rating");
+
+    ratingCard.classList.add("hidden");
+    resultCard.classList.remove("hidden");
+
+    result.textContent = `You selected ${rating} out of 5`;
 });
